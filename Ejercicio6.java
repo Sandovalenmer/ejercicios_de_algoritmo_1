c2efd485dcba1b53e569f6656573a458fc96f53b
Sandovalenmer <valiti19@gmail.com>
2021-06-09 15:36:09 -0600

    Ejercicio 6 en java

---
+++ Ejercicio6.java
@@ -0,0 +1,14 @@
+
+
+public class Ejercicio6{
+
+	public static void main(String[] args){
+	
+	Double n =(Math.random()*200);
+	double aum30;
+
+		System.out.println("El numero aleatorio generado es: "+n);
+		aum30=(n+(n*0.30));
+		System.out.println("El numero aumentado en 30% es: "+aum30);
+	}
+}

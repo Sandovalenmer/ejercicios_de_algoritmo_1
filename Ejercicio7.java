81a69ee7878820081e9ce2ff78315e466c9c2839
Sandovalenmer <valiti19@gmail.com>
2021-06-09 15:36:58 -0600

    Ejercicio 7 en java

---
+++ Ejercicio7.java
@@ -0,0 +1,14 @@
+
+
+public class Ejercicio7{
+
+	public static void main(String[] args){
+	
+	Double n =(Math.random()*(50)+10);
+	double dism30;
+
+		System.out.println("El numero aleatorio generado es: "+n);
+		dism30=(n-(n*0.15));
+		System.out.println("El numero disminuirdo un 15% es: "+dism30);
+	}
+}

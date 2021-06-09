93df50c44ac84f65339924ccb041a1ab3009a30d
Sandovalenmer <valiti19@gmail.com>
2021-06-09 15:31:37 -0600

    Ejercicio1 en java

---
+++ Ejercicio1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Ejercicio1{
+
+	public static void main(String[] args) {
+		int num1, num2, suma;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Ingrese el primer número");
+		num1 = scanner.nextInt();
+		System.out.println("\n");
+		System.out.println("Ingrese el segundo número");
+		num2 = scanner.nextInt();
+		System.out.println("\n");
+
+		suma = num1 + num2;
+
+		System.out.println("El resultado final de la suma es: "+suma);
+
+	
+	}
+
+		
+}
